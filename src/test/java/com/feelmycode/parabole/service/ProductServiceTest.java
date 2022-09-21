760b5186442fe7dfac1cec6f9621a2e8bd509f0d
chd830@naver.com <chd830@naver.com>
2022-09-21 16:15:09 +0900

    product save, update 테스트 코드 추가 및 수정 [#14]

--- src/test/java/com/feelmycode/parabole/service/ProductServiceTest.java
+++ src/test/java/com/feelmycode/parabole/service/ProductServiceTest.java
@@ -28,13 +28,36 @@ class ProductServiceTest {
 
         StringBuilder sb = new StringBuilder();
         sb.append(product.getSellers().getId()).append("\t").append(product.getProductSalesStatus()).append("\t").append(product.getProductRemains());
+        System.out.println("GIVEN");
+        System.out.println(sb.toString());
         // when
-        productService.saveOrUpdateProduct(product);
+        productService.saveProduct(product);
 
         // then
         Product getProduct = productService.getProduct(product.getId());
         sb = new StringBuilder();
         sb.append(getProduct.getSellers().getId()).append("\t").append(getProduct.getProductSalesStatus()).append("\t").append(getProduct.getProductRemains());
+        System.out.println("THEN");
+        System.out.println(sb.toString());
+        assertEquals("상품 상태", product.getProductSalesStatus(), getProduct.getProductSalesStatus());
+        assertEquals("상품 수량", product.getProductRemains(), getProduct.getProductRemains());
+        assertEquals("상품 카테고리", product.getProductCategory(), getProduct.getProductCategory());
+        assertEquals("상품 썸네일", product.getProductThumbnailImg(), getProduct.getProductThumbnailImg());
+        assertEquals("상품 이름", "순대국밥", getProduct.getProductName());
+        assertEquals("상품 가격", product.getProductPrice(), getProduct.getProductPrice());
+    }
+
+    @Test
+    public void updateTest() throws Exception {
+        // given
+        Product product = new Product(1L, new Seller(1), 1, 50L, "국밥", "https://img.url", "순대국밥", 2000L);
+        System.out.println(product.getId()+"\t"+product.getProductPrice()+"\t"+product.getProductRemains());
+        // when
+        Long getId = productService.updateProduct(product);
+
+        //then
+        Product getProduct = productService.getProduct(getId);
+        System.out.println(getProduct.getId()+"\t"+getProduct.getProductPrice()+"\t"+getProduct.getProductRemains());
 
         assertEquals("상품 상태", product.getProductSalesStatus(), getProduct.getProductSalesStatus());
         assertEquals("상품 수량", product.getProductRemains(), getProduct.getProductRemains());
@@ -43,4 +66,5 @@ class ProductServiceTest {
         assertEquals("상품 이름", "순대국밥", getProduct.getProductName());
         assertEquals("상품 가격", product.getProductPrice(), getProduct.getProductPrice());
     }
+
 }
