2b222221f7c402c4dfc8105698c96514cd8cbb59
Daeun Chung <46639550+daeunchung@users.noreply.github.com>
2022-09-26 20:59:06 +0900

    Feat: User SignUp, SignIn API

--- src/main/java/com/feelmycode/parabole/repository/UserRepository.java
+++ src/main/java/com/feelmycode/parabole/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.feelmycode.parabole.repository;
 
 import com.feelmycode.parabole.domain.User;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -8,6 +9,9 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+    Optional<User> findByPassword(String password);
 
 }
 
