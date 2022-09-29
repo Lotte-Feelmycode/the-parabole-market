4dfb58e59b2eb005c9149190ad3437169ee55bfd
IMHYEWON <hyewon1118@gmail.com>
2022-09-29 17:38:15 +0900

    Refactor: ToString, 사용 안하는 import 제거

--- src/main/java/com/feelmycode/parabole/dto/EventCreateRequestDto.java
+++ src/main/java/com/feelmycode/parabole/dto/EventCreateRequestDto.java
@@ -4,7 +4,6 @@ import com.feelmycode.parabole.domain.EventImage;
 import java.time.LocalDateTime;
 import java.util.List;
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
