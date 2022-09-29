3b49f02b3c14791242cad3aa55693bd8fcf3ca46
Eun chong <chd830@naver.com>
2022-09-29 12:07:11 +0900

    Fix: userId로 주문정보 가져오는 기능 수정 [#1]

--- src/main/java/com/feelmycode/parabole/repository/OrderRepository.java
+++ src/main/java/com/feelmycode/parabole/repository/OrderRepository.java
@@ -1,12 +1,12 @@
 package com.feelmycode.parabole.repository;
 
 import com.feelmycode.parabole.domain.Order;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
-    Order findByUserIdOrderByOrderIdDesc(Long userId);
+    Order findTopByUserIdOrderByIdDesc(Long userId);
 
 }
