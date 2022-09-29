5a9503f13d631a056e73fe7c5ed9cdd88beb8e4b
IMHYEWON <hyewon1118@gmail.com>
2022-09-29 15:17:42 +0900

    Fix: event API url 단수로 수정

--- src/main/java/com/feelmycode/parabole/controller/EventController.java
+++ src/main/java/com/feelmycode/parabole/controller/EventController.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/events")
+@RequestMapping("/api/v1/event")
 public class EventController {
 
     private final EventService eventService;
