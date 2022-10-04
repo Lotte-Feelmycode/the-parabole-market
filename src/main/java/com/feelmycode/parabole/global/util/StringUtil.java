a045f38e6d9b722548ec92c46682ca3ea5001998
Kenneth Jung <jungeu1509@gmail.com>
2022-10-04 17:40:10 +0900

    Fix: String check util 간소화
    Co-authored-by: Eun chong <chd830@naver.com>

--- src/main/java/com/feelmycode/parabole/global/util/StringUtil.java
+++ src/main/java/com/feelmycode/parabole/global/util/StringUtil.java
@@ -3,8 +3,7 @@ package com.feelmycode.parabole.global.util;
 public class StringUtil {
 
     public static boolean controllerParamIsBlank(String inputString) {
-        if(inputString == null || inputString.isEmpty() || inputString.isBlank()) return true;
-        return inputString.equals("null");
+ return inputString == null || inputString.isEmpty() || inputString.isBlank() || inputString.equals("null");
     }
 
 }
