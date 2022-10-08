a304c7436e3a0298fc8d0d3b74b5c4a3da4b300f
Kenneth Jung <jungeu1509@gmail.com>
2022-10-08 14:53:22 +0900

    Fix: run.sh 오류 수정

--- script/run.sh
+++ script/run.sh
@@ -3,36 +3,35 @@
 PROJECT_NAME="The Parabole"
 
 TODAY=$(date +"%Y%m%d")
+
 DEPLOY_PATH=/home/ubuntu
+DEPLOY_JAR=parabole-0.0.1-SNAPSHOT.jar
+DEPLOY_FILE=$DEPLOY_PATH/$DEPLOY_JAR
 
 LOG_PATH=$DEPLOY_PATH/log
-LINK_LOG_FILE=DEPLOY_PATH/parabole.log
+LINK_LOG_FILE=$LOG_PATH/parabole.log
 LOG_FILE=$LOG_PATH/$PROJECT_NAME_$TODAY.log
 ERR_LOG_FILE=$LOG_PATH/$PROJECT_NAME_ERROR_$TODAY.log
-DEPLOY_JAR=$parabole-0.0.1-SNAPSHOT.jar
 
-echo "> Start run.sh for $PROJECT_NAME" >> $LOG_FILE
+echo "> Start run.sh for $PROJECT_NAME : $TODAY" >> $LOG_FILE
 
-echo "> Delete log link" >> $LOG_FILE
+echo "> Delete log link file" >> $LOG_FILE
 rm $LINK_LOG_FILE
-ln - s $LOG_FILE $LINK_LOG_FILE
+ln -s $LOG_FILE $LINK_LOG_FILE
 echo "> Build log : $LOG_FILE" >> $LOG_FILE
-
 
 echo "> Check $PROJECT_NAME running"
 CURRENT_PID=$(ps -ef | grep java | grep parabole | awk '{print $2}')
-echo "$CURRENT_PID"
 
 if [ -z $CURRENT_PID ]
 then
   echo "> No such current pid" >> $LOG_FILE
 else
- echo "> kill -9 $CURRENT_PID"
- kill -9 $CURRENT_PID
- sleep 10
+  echo "> kill -9 $CURRENT_PID" >> $LOG_FILE
+  kill -9 $CURRENT_PID
+  sleep 10
 fi
 
-DEPLOY_FILE=$DEPLOY_PATH/$DEPLOY_JAR
 echo "> Deploy start : $DEPLOY_JAR"
 nohup java -jar $DEPLOY_FILE >> LOG_FILE 2>$ERR_LOG_FILE &
 
