bbdc07a1989c2b2f9af77dd3e8295bae05f88937
Kenneth Jung <52902010+jungeu1509@users.noreply.github.com>
2022-10-14 12:11:31 +0900

    Fix: cart가 사용자와 매핑되도록 변경, 장바구니 생성시 로직 변경

--- src/main/java/com/feelmycode/parabole/service/CartService.java
+++ src/main/java/com/feelmycode/parabole/service/CartService.java
@@ -1,30 +1,37 @@
 package com.feelmycode.parabole.service;
 
 import com.feelmycode.parabole.domain.Cart;
+import com.feelmycode.parabole.domain.User;
 import com.feelmycode.parabole.global.error.exception.ParaboleException;
 import com.feelmycode.parabole.repository.CartRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class CartService {
 
     private final CartRepository cartRepository;
+    private final UserService userService;
 
     public Long createCart(Long userId) {
-        Cart cart = getCart(userId);
-        cartRepository.save(cart);
-        return cart.getId();
+        checkNoCart(userId);
+        User user = userService.getUser(userId);
+        Cart cart = new Cart(user);
+        Cart save = cartRepository.save(cart);
+        return save.getId();
     }
 
     public Cart getCart(Long userId) {
-        Cart cart = cartRepository.findByUserId(userId);
-        if (cart != null) {
-            throw new ParaboleException(HttpStatus.BAD_REQUEST, "장바구니가 이미 있습니다.");
-        } else {
-            return cart;
-        }
+        return cartRepository.findByUserId(userId).orElseThrow(()->
+            new ParaboleException(HttpStatus.BAD_REQUEST, "장바구니가 없습니다."));
+    }
+
+    public void checkNoCart(Long userId) {
+        cartRepository.findByUserId(userId)
+            .ifPresent(u -> {throw new ParaboleException(HttpStatus.BAD_REQUEST, "장바구니가 이미 있습니다.");});
     }
 }
