fb33b52e568e7387060d9bc93d34d11c5e7104e1
Eun chong <chd830@naver.com>
2022-10-24 09:35:46 +0900

    Feat: REST assured를 사용하는 dependency추가 및 기본 컨트롤러 선언 [#44]

---
+++ src/test/java/com/feelmycode/parabole/controller/BaseControllerTest.java
@@ -0,0 +1,42 @@
+package com.feelmycode.parabole.controller;
+
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.documentationConfiguration;
+
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@ExtendWith(RestDocumentationExtension.class)
+class BaseController {
+
+    protected static final String DEFAULT_RESTDOC_PATH = "{class_name}/{method_name}/";
+    protected RequestSpecification spec;
+
+    @LocalServerPort
+    int port;
+
+    @BeforeEach
+    void setUp() {
+        RestAssured.port = port;
+    }
+
+    @BeforeEach
+    void setUpRestDocs(RestDocumentationContextProvider restDocumentation) {
+        this.spec = new RequestSpecBuilder()
+            .setPort(port)
+            .addFilter(documentationConfiguration(restDocumentation)
+                .operationPreprocessors()
+                .withRequestDefaults(prettyPrint())
+                .withResponseDefaults(prettyPrint()))
+            .build();
+    }
+}
