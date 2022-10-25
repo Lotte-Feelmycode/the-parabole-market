63510c52ac16317df6fe8ab94170f1aa79037edb
IMHYEWON <hyewon1118@gmail.com>
2022-10-25 15:05:53 +0900

    Feat: 이벤트 목록 조회 수정

--- src/main/java/com/feelmycode/parabole/repository/EventRepository.java
+++ src/main/java/com/feelmycode/parabole/repository/EventRepository.java
@@ -15,9 +15,7 @@ public interface EventRepository extends JpaRepository<Event, Long> {
     List<Event> findAllByIsDeleted(boolean isDeleted);
     List<Event> findAllBySellerAndIsDeleted(Seller seller, boolean isDeleted);
     List<Event> findAllBySellerIdOrderByStartAtAsc(Long SellerId);
-    List<Event> findAllByTypeAndIsDeleted(String eventType, boolean isDeleted);
-    List<Event> findAllByTitleContainingAndIsDeleted(String eventTitle, boolean isDeleted);
     List<Event> findAllByStartAtBetweenAndIsDeleted(LocalDateTime startDateTime, LocalDateTime endDateTime, boolean isDeleted);
     List<Event> findAllByEndAtBetweenAndIsDeleted(LocalDateTime startDateTime, LocalDateTime endDateTime, boolean isDeleted);
-    List<Event> findAllByStatusAndIsDeleted(Integer eventStatus, boolean isDeleted);
+    List<Event> findAllByTypeAndStatusAndTitleContainingAndIsDeleted(String eventType, Integer eventStauts, String eventTitle, boolean isDeleted);
 }
