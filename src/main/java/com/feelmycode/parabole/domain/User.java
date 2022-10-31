12c3fa276335a1cd7a6fa349b2091d0b1ac4fbd2
Kenneth Jung <52902010+jungeu1509@users.noreply.github.com>
2022-10-31 20:08:09 +0900

    !HOTFIX: 3ce32b 버전으로 되돌리기
    3ce32bf7071c03a781a2b970ba54275963798b4a

--- src/main/java/com/feelmycode/parabole/domain/User.java
+++ src/main/java/com/feelmycode/parabole/domain/User.java
@@ -1,6 +1,8 @@
 package com.feelmycode.parabole.domain;
 
 import com.feelmycode.parabole.global.error.exception.NotSellerException;
+import com.feelmycode.parabole.global.error.exception.ParaboleException;
+import com.sun.istack.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.Column;
@@ -12,7 +14,6 @@ import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -27,37 +28,28 @@ public class User extends BaseEntity {
     @Column(name = "user_id")
     private Long id;
 
-    @Column(name = "user_email", length = 200)//, nullable = false)
-//    @NotNull
+    @Column(name = "user_email", length = 200)
+    @NotNull
     private String email;
 
     @Column(name = "user_name", length = 200)
-//    @NotNull
-    private String username;
+    @NotNull
+    private String name;
 
     @Column(name = "user_nickname", length = 200)
-//    @NotNull
+    @NotNull
     private String nickname;
 
     @Column(name = "user_phone", length = 200)
-//    @NotNull
+    @NotNull
     private String phone;
 
     @Column(name = "user_password", length = 200)
-//    @NotNull
+    @NotNull
     private String password;
 
-    @Column(name = "user_role", length = 200)
-    private String role;
-
-    @Column(name = "user_img_url", length = 200)
-    private String imageUrl;
-
-    @Column(name = "user_auth_provider", length = 200)
-    private String authProvider; // example : facebook
-
     @JoinColumn(name = "seller_id")
-//    @NotNull
+    @NotNull
     @OneToOne
     private Seller seller;
 
@@ -87,28 +79,9 @@ public class User extends BaseEntity {
     public User(String email, String name,
         String nickname, String phone, String password) {
         this.email = email;
-        this.username = name;
+        this.name = name;
         this.nickname = nickname;
         this.phone = phone;
         this.password = password;
     }
-
-    @Builder
-    public User(Long id, String email, String username, String nickname, String phone,
-        String password,
-        String role, String imageUrl, String authProvider, Seller seller,
-        List<UserCoupon> userCoupons) {
-        this.id = id;
-        this.email = email;
-        this.username = username;
-        this.nickname = nickname;
-        this.phone = phone;
-        this.password = password;
-        this.role = role;
-        this.imageUrl = imageUrl;
-        this.authProvider = authProvider;
-        this.seller = seller;
-        this.userCoupons = userCoupons;
-    }
-
 }
