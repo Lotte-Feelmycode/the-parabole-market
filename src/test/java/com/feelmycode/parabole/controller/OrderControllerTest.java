3f79d6152872d1db64b6cfa3129f5b51b1043a3b
Eun chong <chd830@naver.com>
2022-10-31 11:10:32 +0900

    Docs: REST DOCS 문서 수정. type(JsonFieldType)추가 [#168]

--- src/test/java/com/feelmycode/parabole/controller/OrderControllerTest.java
+++ src/test/java/com/feelmycode/parabole/controller/OrderControllerTest.java
@@ -6,6 +6,7 @@ import static org.springframework.restdocs.operation.preprocess.Preprocessors.pr
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
 import static org.springframework.restdocs.request.RequestDocumentation.requestParameters;
@@ -26,6 +27,7 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.restdocs.JUnitRestDocumentation;
+import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -62,9 +64,9 @@ public class OrderControllerTest {
                     prettyPrint()),
                 preprocessResponse(prettyPrint()),
                 responseFields(
-                    fieldWithPath("success").description("성공여부"),
-                    fieldWithPath("message").description("메세지"),
-                    fieldWithPath("data").description("주문 수정 정보")
+                    fieldWithPath("success").type(JsonFieldType.BOOLEAN).description("성공여부"),
+                    fieldWithPath("message").type(JsonFieldType.STRING).description("메세지"),
+                    fieldWithPath("data").type(JsonFieldType.OBJECT).description("주문 수정 정보")
                 )
             ))
             .when()
@@ -89,9 +91,9 @@ public class OrderControllerTest {
                     prettyPrint()),
                 preprocessResponse(prettyPrint()),
                 responseFields(
-                    fieldWithPath("success").description("성공여부"),
-                    fieldWithPath("message").description("메세지"),
-                    fieldWithPath("data").description("주문 수정 정보")
+                    fieldWithPath("success").type(JsonFieldType.BOOLEAN).description("성공여부"),
+                    fieldWithPath("message").type(JsonFieldType.STRING).description("메세지"),
+                    fieldWithPath("data").type(JsonFieldType.OBJECT).description("주문 수정 정보")
                 )
             ))
             .when()
@@ -112,24 +114,24 @@ public class OrderControllerTest {
                         .host("parabole.com"),
                     prettyPrint()),
                 preprocessResponse(prettyPrint()),
-                requestParameters(
-                    parameterWithName("userId").description("사용자 ID")
+                requestFields(
+                    fieldWithPath("userId").type(JsonFieldType.NUMBER).description("사용자 ID")
                 ),
                 responseFields(
-                    fieldWithPath("success").description("성공여부"),
-                    fieldWithPath("message").description("메세지"),
-                    fieldWithPath("data").description("주문 정보"),
-                    fieldWithPath("data.[].id").description("상품 아이디"),
-                    fieldWithPath("data.[].state").description("상품 명"),
-                    fieldWithPath("data.[].userId").description("사용자 아이디"),
-                    fieldWithPath("data.[].userEmail").description("사용자 이메일"),
-                    fieldWithPath("data.[].productId").description("상품 ID"),
-                    fieldWithPath("data.[].productName").description("상품 명"),
-                    fieldWithPath("data.[].productCnt").description("상품 개수"),
-                    fieldWithPath("data.[].productRemain").description("상품 재고"),
-                    fieldWithPath("data.[].productPrice").description("상품 가격"),
-                    fieldWithPath("data.[].productDiscountPrice").description("상품 할인 가격"),
-                    fieldWithPath("data.[].productThumbnailImg").description("상품 썸네일 이미지")
+                    fieldWithPath("success").type(JsonFieldType.BOOLEAN).description("성공여부"),
+                    fieldWithPath("message").type(JsonFieldType.STRING).description("메세지"),
+                    fieldWithPath("data").type(JsonFieldType.OBJECT).description("주문 정보"),
+                    fieldWithPath("data.[].id").type(JsonFieldType.NUMBER).description("상품 아이디"),
+                    fieldWithPath("data.[].state").type(JsonFieldType.STRING).description("상품 명"),
+                    fieldWithPath("data.[].userId").type(JsonFieldType.NUMBER).description("사용자 아이디"),
+                    fieldWithPath("data.[].userEmail").type(JsonFieldType.STRING).description("사용자 이메일"),
+                    fieldWithPath("data.[].productId").type(JsonFieldType.NUMBER).description("상품 ID"),
+                    fieldWithPath("data.[].productName").type(JsonFieldType.STRING).description("상품 명"),
+                    fieldWithPath("data.[].productCnt").type(JsonFieldType.NUMBER).description("상품 개수"),
+                    fieldWithPath("data.[].productRemain").type(JsonFieldType.NUMBER).description("상품 재고"),
+                    fieldWithPath("data.[].productPrice").type(JsonFieldType.NUMBER).description("상품 가격"),
+                    fieldWithPath("data.[].productDiscountPrice").type(JsonFieldType.NUMBER).description("상품 할인 가격"),
+                    fieldWithPath("data.[].productThumbnailImg").type(JsonFieldType.STRING).description("상품 썸네일 이미지")
                 )
             ))
             .when()
--- src/test/java/com/feelmycode/parabole/controller/OrderInfoControllerTest.java
+++ src/test/java/com/feelmycode/parabole/controller/OrderInfoControllerTest.java
@@ -6,6 +6,7 @@ import static org.springframework.restdocs.operation.preprocess.Preprocessors.pr
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
 import static org.springframework.restdocs.request.RequestDocumentation.requestParameters;
@@ -29,6 +30,7 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.restdocs.JUnitRestDocumentation;
+import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -70,16 +72,10 @@ public class OrderInfoControllerTest {
                         .host("parabole.com"),
                     prettyPrint()),
                 preprocessResponse(prettyPrint()),
-                requestParameters(
-//                    parameterWithName("userId").description("상품 ID"),
-//                    parameterWithName("[]").description("상세 주문 정보"),
-//                    parameterWithName("[].productId").description("상품 ID"),
-//                    parameterWithName("[].productCnt").description("상품 개수")
-                ),
                 responseFields(
-                    fieldWithPath("success").description("성공여부"),
-                    fieldWithPath("message").description("메세지"),
-                    fieldWithPath("data").description("주문 수정 정보")
+                    fieldWithPath("success").type(JsonFieldType.BOOLEAN).description("성공여부"),
+                    fieldWithPath("message").type(JsonFieldType.STRING).description("메세지"),
+                    fieldWithPath("data").type(JsonFieldType.OBJECT).description("주문 수정 정보")
                 )
             ))
             .when()
@@ -100,25 +96,24 @@ public class OrderInfoControllerTest {
                         .host("parabole.com"),
                     prettyPrint()),
                 preprocessResponse(prettyPrint()),
-                requestParameters(
-                    parameterWithName("userId").description("사용자 이름")
+                requestFields(
+                    fieldWithPath("userId").type(JsonFieldType.STRING).description("사용자 이름")
                 ),
                 responseFields(
-                    fieldWithPath("success").description("성공여부"),
-                    fieldWithPath("message").description("메세지"),
-                    fieldWithPath("data").description("주문 수정 정보"),
-                    fieldWithPath("data").description("주문 정보 목록"),
-                    fieldWithPath("data.[].id").description("상품 아이디"),
-                    fieldWithPath("data.[].state").description("상품 명"),
-                    fieldWithPath("data.[].userId").description("사용자 아이디"),
-                    fieldWithPath("data.[].userEmail").description("사용자 이메일"),
-                    fieldWithPath("data.[].productId").description("상품 ID"),
-                    fieldWithPath("data.[].productName").description("상품 명"),
-                    fieldWithPath("data.[].productCnt").description("상품 개수"),
-                    fieldWithPath("data.[].productRemain").description("상품 재고"),
-                    fieldWithPath("data.[].productPrice").description("상품 가격"),
-                    fieldWithPath("data.[].productDiscountPrice").description("상품 할인 가격"),
-                    fieldWithPath("data.[].productThumbnailImg").description("상품 썸네일 이미지")
+                    fieldWithPath("success").type(JsonFieldType.BOOLEAN).description("성공여부"),
+                    fieldWithPath("message").type(JsonFieldType.STRING).description("메세지"),
+                    fieldWithPath("data").type(JsonFieldType.STRING).description("주문 정보 목록"),
+                    fieldWithPath("data.[].id").type(JsonFieldType.NUMBER).description("상품 아이디"),
+                    fieldWithPath("data.[].state").type(JsonFieldType.STRING).description("상품 명"),
+                    fieldWithPath("data.[].userId").type(JsonFieldType.NUMBER).description("사용자 아이디"),
+                    fieldWithPath("data.[].userEmail").type(JsonFieldType.STRING).description("사용자 이메일"),
+                    fieldWithPath("data.[].productId").type(JsonFieldType.STRING).description("상품 ID"),
+                    fieldWithPath("data.[].productName").type(JsonFieldType.STRING).description("상품 명"),
+                    fieldWithPath("data.[].productCnt").type(JsonFieldType.NUMBER).description("상품 개수"),
+                    fieldWithPath("data.[].productRemain").type(JsonFieldType.NUMBER).description("상품 재고"),
+                    fieldWithPath("data.[].productPrice").type(JsonFieldType.NUMBER).description("상품 가격"),
+                    fieldWithPath("data.[].productDiscountPrice").type(JsonFieldType.NUMBER).description("상품 할인 가격"),
+                    fieldWithPath("data.[].productThumbnailImg").type(JsonFieldType.STRING).description("상품 썸네일 이미지")
                 )
             ))
             .when()
@@ -141,9 +136,9 @@ public class OrderInfoControllerTest {
                     prettyPrint()),
                 preprocessResponse(prettyPrint()),
                 responseFields(
-                    fieldWithPath("success").description("성공여부"),
-                    fieldWithPath("message").description("메세지"),
-                    fieldWithPath("data").description("주문 수정 정보")
+                    fieldWithPath("success").type(JsonFieldType.BOOLEAN).description("성공여부"),
+                    fieldWithPath("message").type(JsonFieldType.STRING).description("메세지"),
+                    fieldWithPath("data").type(JsonFieldType.OBJECT).description("주문 수정 정보")
                 )
             ))
             .when()
