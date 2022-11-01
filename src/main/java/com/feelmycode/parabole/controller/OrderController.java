6a2a9d2e66315a9013ad6c50dfdc18bbf069287d
Eun chong <chd830@naver.com>
2022-11-01 16:42:40 +0900

    Feat: Order Controller와 OrderInfo Controller에서 다르게 동작하는 코드 복구 [#174]

--- src/main/java/com/feelmycode/parabole/controller/OrderController.java
+++ src/main/java/com/feelmycode/parabole/controller/OrderController.java
@@ -1,19 +1,24 @@
 package com.feelmycode.parabole.controller;
 
+import com.feelmycode.parabole.domain.Order;
 import com.feelmycode.parabole.dto.OrderDeliveryUpdateRequestDto;
+import com.feelmycode.parabole.dto.OrderInfoResponseDto;
 import com.feelmycode.parabole.dto.OrderUpdateRequestDto;
 import com.feelmycode.parabole.global.api.ParaboleResponse;
 import com.feelmycode.parabole.service.OrderInfoService;
 import com.feelmycode.parabole.service.OrderService;
 import com.feelmycode.parabole.service.UpdateService;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
@@ -38,4 +43,10 @@ public class OrderController {
         return ParaboleResponse.CommonResponse(HttpStatus.OK, true, "주문 정보 저장 완료");
     }
 
+    @GetMapping
+    public ResponseEntity<ParaboleResponse> getOrderList(@RequestParam Long userId) {
+        List<Order> orderList = orderService.getOrderList(userId);
+        List<OrderInfoResponseDto> orderInfoResponseDtoList = orderInfoService.getOrderInfoListByUserId(orderList);
+        return ParaboleResponse.CommonResponse(HttpStatus.OK, true, "모든 주문 정보 호출", orderInfoResponseDtoList);
+    }
 }
