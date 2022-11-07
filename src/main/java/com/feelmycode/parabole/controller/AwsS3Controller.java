565dc27bef375c25b2b4028e944d31bec59b9f63
Eun chong <chd830@naver.com>
2022-11-07 20:59:37 +0900

    Feat: AWS 설정 추가[#153]

---
+++ src/main/java/com/feelmycode/parabole/controller/AwsS3Controller.java
@@ -0,0 +1,65 @@
+package com.feelmycode.parabole.controller;
+
+import com.feelmycode.parabole.domain.ProductDetail;
+import com.feelmycode.parabole.global.api.ParaboleResponse;
+import com.feelmycode.parabole.service.AwsS3Service;
+import com.feelmycode.parabole.service.ProductDetailService;
+import com.feelmycode.parabole.service.ProductService;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@CrossOrigin(origins = {"http://localhost:3000"})
+@RequestMapping("/api/v1/s3")
+public class AwsS3Controller {
+
+    private final AwsS3Service awsS3Service;
+    private final ProductDetailService productDetailService;
+    private final ProductService productService;
+
+    /**
+     * Amazon S3에 이미지 업로드
+     *
+     * @return 성공 시 200 Success와 함께 업로드 된 파일의 파일명 리스트 반환
+     */
+    @PostMapping
+    public ResponseEntity<ParaboleResponse> uploadImage(@RequestParam() Long productId,
+        @RequestPart("images") List<MultipartFile> multipartFile) throws Exception {
+        log.info("productId: {}", productId);
+
+        log.info("UPLOAD IMAGE size: {}", multipartFile == null ? 0 : multipartFile.size());
+//        log.info("UPLOAD IMAGE: {}", multipartFile);
+        if (multipartFile != null) {
+            for (MultipartFile file : multipartFile) {
+                String imgUrl = awsS3Service.upload(file);
+                productDetailService.createProductDetail(
+                    1L, new ProductDetail(productService.getProduct(productId), imgUrl, ""));
+            }
+        }
+        return ParaboleResponse.CommonResponse(HttpStatus.OK, true, "이미지 업로드");
+    }
+
+    /**
+     * Amazon S3에 이미지 업로드 된 파일을 삭제
+     *
+     * @return 성공 시 200 Success
+     */
+    @DeleteMapping
+    public ResponseEntity<ParaboleResponse> deleteImage(@RequestParam String fileName) {
+//        awsS3Service.deleteImage(fileName);
+        return ParaboleResponse.CommonResponse(HttpStatus.OK, true, "파일 삭제");
+    }
+}
---
+++ src/main/java/com/feelmycode/parabole/service/AwsS3Service.java
@@ -0,0 +1,75 @@
+package com.feelmycode.parabole.service;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import java.io.IOException;
+import java.util.List;
+import java.util.UUID;
+import javax.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class AwsS3Service {
+
+    private AmazonS3 amazonS3;
+
+    @Value("${cloud.aws.credentials.accessKey}")
+    private String accessKey;
+    @Value("${cloud.aws.credentials.secretKey}")
+    private static String secretKey;
+    @Value("${cloud.aws.region.static}")
+    private static String region;
+    @Value("${cloud.aws.s3.bucket}")
+    private static String bucket;
+
+    @PostConstruct
+    public void setS3Client() {
+        AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
+
+        amazonS3 = AmazonS3ClientBuilder.standard()
+            .withCredentials(new AWSStaticCredentialsProvider(credentials))
+            .withRegion(region)
+            .build();
+    }
+
+    public String upload(MultipartFile file) throws IOException {
+        String fileName = createFileName(file.getOriginalFilename());
+
+        amazonS3.putObject(new PutObjectRequest(bucket, fileName, file.getInputStream(), null)
+            .withCannedAcl(CannedAccessControlList.PublicRead));
+        return amazonS3.getUrl("parabole-bucket", fileName).toString();
+
+    }
+
+    private String createFileName(String originalFileName) { //파일이름 유니크하게
+        return UUID.randomUUID().toString().concat(getFileExtension(originalFileName));
+    }
+
+    private String getFileExtension(String fileName) {
+        try {
+            return fileName.substring(fileName.lastIndexOf("."));
+        } catch (StringIndexOutOfBoundsException e) {
+            throw new IllegalArgumentException(String.format("잘못된 형식의 파일 (%s) 입니다", fileName));
+        }
+    }
+
+    public String[] upload(List<MultipartFile> files) throws IOException {
+        String[] str = new String[files.size()];
+        int i = 0;
+        for (MultipartFile file : files) {
+            str[i++] = upload(file);
+        }
+        return str;
+    }
+}
