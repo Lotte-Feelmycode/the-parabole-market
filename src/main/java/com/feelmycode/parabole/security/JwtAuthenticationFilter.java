64dbe395921f2256df1a7bdcc115750bc89609ca
Daeun Chung <46639550+daeunchung@users.noreply.github.com>
2022-11-07 10:08:17 +0900

    Feat: JwtUtils와 해독 Filter 추가

--- src/main/java/com/feelmycode/parabole/security/JwtAuthenticationFilter.java
+++ src/main/java/com/feelmycode/parabole/security/JwtAuthenticationFilter.java
@@ -1,6 +1,7 @@
 package com.feelmycode.parabole.security;
 
-import com.feelmycode.parabole.security.utils.TokenProvider;
+import com.feelmycode.parabole.security.utils.JwtUtils;
+import io.jsonwebtoken.Claims;
 import java.io.IOException;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -8,12 +9,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AbstractAuthenticationToken;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.authority.AuthorityUtils;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -21,40 +16,47 @@ import org.springframework.web.filter.OncePerRequestFilter;
 @Slf4j
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
     @Autowired
-    private TokenProvider tokenProvider;
+    private JwtUtils jwtUtils;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+        throws ServletException, IOException {
         String token = null;
         try {
             token = parseBearerToken(request);
+            // 위에서 받은 token 은 내가 만든 customized Jwt token. Which means they are not authenticated by Spring Security
             log.info("Filter is running...");
 
             if (token != null && !token.equalsIgnoreCase("null")) {
-                Long userId = Long.parseLong(tokenProvider.validateAndGetUserId(token));
-                log.info("Authenticated user ID : {}", userId );
+                log.info("Jwt Token 해독결과 userId {} role {} sellerId {} ",
+                    jwtUtils.extractUserId(token), jwtUtils.extractRole(token), jwtUtils.extractSellerId(token));
 
-                AbstractAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
-                    userId,
-                    null,
-                    AuthorityUtils.NO_AUTHORITIES
-                );
-                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-                SecurityContext securityContext = SecurityContextHolder.createEmptyContext();
-                securityContext.setAuthentication(authentication);
-                SecurityContextHolder.setContext(securityContext);
+                Claims claims = jwtUtils.extractAllClaims(token);
+                request.setAttribute("userId", claims.get("userId",Long.class));
+                request.setAttribute("email", claims.get("email",String.class));
+                request.setAttribute("username", claims.get("username",String.class));
+                request.setAttribute("nickname", claims.get("nickname",String.class));
+                request.setAttribute("phone", claims.get("phone",String.class));
+                request.setAttribute("imageUrl", claims.get("imageUrl",String.class));
+                request.setAttribute("role", claims.get("role",String.class));
+                if (claims.get("role", String.class).equals("ROLE_SELLER")) {
+                    request.setAttribute("sellerId", Long.class);
+                    request.setAttribute("sellerStorename", String.class);
+                }
+                request.setAttribute("authProvider", String.class);
+
             }
         } catch (Exception ex) {
-            log.error("Could not set user authentication in security context", ex);
+//            log.error("Could not set user authentication in security context", ex);
+            log.error("Could not Extract Claims and set Request", ex);
         }
-        request.setAttribute("userToken", token);
         filterChain.doFilter(request, response);
     }
 
     private String parseBearerToken(HttpServletRequest request) {
         String bearerToken = request.getHeader("Authorization");
-        log.info("Request : {}", request.toString());
         log.info("parse bearer token : {}", bearerToken);
 
         if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
