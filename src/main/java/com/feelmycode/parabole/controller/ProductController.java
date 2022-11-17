ec5a54e255ecef39884b426ef97d300e2bb20fa7
choihyeongjun <69442251+choihyeongjun@users.noreply.github.com>
2022-11-17 10:28:45 +0900

    Update ProductController.java

--- src/main/java/com/feelmycode/parabole/controller/ProductController.java
+++ src/main/java/com/feelmycode/parabole/controller/ProductController.java
@@ -15,6 +15,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -30,6 +31,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @RequiredArgsConstructor
 @RestController
+@CrossOrigin("*")
 @RequestMapping("/api/v1/product")
 @Slf4j
 public class ProductController {
